3755489488a2c51168e1dd18d06446f9ff6615d2
thomasvargiu <tvargiu@gmail.com>
2020-11-19 18:50:01 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import { pipe, flow } from "fp-ts/function";
 type Deps = {
     multiplier: number
 }
-const f1 = (id: string): R.Reader<number, number> => (multiplier: number) => id.length * multiplier;
+const f1 = (id: string): R.Reader<{multiplier: number}, number> => ({multiplier}) => id.length * multiplier;
 const f2 = (a: number) => a + 1;
 const f3 = (a: number): string => 'baz'.repeat(a);
 
 const factory = (id: string): R.Reader<Deps, string> => pipe(
     R.ask<Deps>(),
-    R.map(({multiplier}) => f1(id)(multiplier)),
+    R.map(f1(id)),
     R.map(f2),
     R.map(f3),
 )
